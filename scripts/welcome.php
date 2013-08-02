0ffc699d30c29edab32cafd55feb08db4624eeba
Roman Bruckner <bruckner.roman@gmail.com>
2013-08-02 17:16:46 +0100

    remove gray box from welcome page

--- scripts/welcome.php
+++ scripts/welcome.php
@@ -1,11 +1,9 @@
-<div class="gray-box">
-    <h1>
-        Welcome To The SellCloudMusic
-    </h1>
-    <h2>What is it?</h2>
-    <p>The SellCloudMusic is an internet shop enabling <a href="https://www.Soundcloud.com">SoundCloud</a> users to conveniently sell their music</p>
-    <h2>Who is it for?</h2>
-    <p>The SellCloudMusic is well fitted for producers with an existing  <a href="https://www.Soundcloud.com">SoundCloud</a> account or anyone who is making music with an intension to sell it.</p>
-    <h2>Who am I?</h2>
-    <p>My name is Roman Bruckner and I made this as a part of my bachelor thesis.</p>
-</div>
+<h1>
+    Welcome To The SellCloudMusic
+</h1>
+<h2>What is it?</h2>
+<p>The SellCloudMusic is an internet shop enabling <a href="https://www.Soundcloud.com">SoundCloud</a> users to conveniently sell their music</p>
+<h2>Who is it for?</h2>
+<p>The SellCloudMusic is well fitted for producers with an existing  <a href="https://www.Soundcloud.com">SoundCloud</a> account or anyone who is making music with an intension to sell it.</p>
+<h2>Who am I?</h2>
+<p>My name is Roman Bruckner and I made this as a part of my bachelor thesis.</p>
