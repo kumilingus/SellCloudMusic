3719e299c2f91f6369e7138e7d64fafad1d29673
Roman Bruckner <bruckner.roman@gmail.com>
2013-08-03 16:03:02 +0100

    remove gray bobx from thankyou page

--- scripts/thankyou.php
+++ scripts/thankyou.php
@@ -1,8 +1,7 @@
-<div class="gray-box">
-    <h1>
-        Thank You for shopping on SellCloudMusic.
-    </h1>
-    <p>You will recieve a confirmation email shortly.</p>
-</div>
+<h1>
+    Thank You for shopping on SellCloudMusic.
+</h1>
+<p>You will recieve a confirmation email shortly.</p>
+<p>Your email is the PayPal one.</p>
 <script type="text/javascript" src="lib/paypal/minicart.js"></script>
 <script type="text/javascript">PAYPAL.apps.MiniCart.reset()</script>
