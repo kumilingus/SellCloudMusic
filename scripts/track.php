a33fc50e38bdda73e88d99d925e6dfdee72c275a
Roman Bruckner <bruckner.roman@gmail.com>
2013-08-15 22:20:56 +0100

    rename config properties host and sellcloudmusic-url

--- scripts/track.php
+++ scripts/track.php
@@ -13,9 +13,9 @@ if ($conn->loadEntity($track) && $track->getID() > 0 && !$track->isSoldOut()) {
 
         $trackFrm = new Form($track, array(
             "action" => "https://www.sandbox.paypal.com/cgi-bin/webscr",
-            "cancel_url" => Config::_('server-name').$_SERVER['REQUEST_URI'],
+            "cancel_url" => Config::_('host').$_SERVER['REQUEST_URI'],
             "paypal_account" => $user->paypal_email,
-            "return_url" => Config::_('host').'/?thankyou'
+            "return_url" => Config::_('sellcloudmusic-url').'/?thankyou'
         ));
 
         echo $trackFrm->toHTML();
