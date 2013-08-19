7189f6696270067551eccc550c675078bc861299
Roman Bruckner <bruckner.roman@gmail.com>
2013-08-19 10:52:08 +0100

    fix type in welcome screen

--- scripts/welcome.php
+++ scripts/welcome.php
@@ -6,4 +6,4 @@
 <h2>Who is it for?</h2>
 <p>The SellCloudMusic is well fitted for producers with an existing  <a href="https://www.Soundcloud.com">SoundCloud</a> account or anyone who is making music with an intension to sell it.</p>
 <h2>Who am I?</h2>
-<p>My name is Roman Bruckner and I made this as a part of my bachelor thesis.</p>
+<p>My name is Roman Bruckner and I made this as a part of my bachelor theses.</p>
